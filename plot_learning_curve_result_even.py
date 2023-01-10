633cbb5d11f0b52bb57f7b20e5f3684abbfc0670
kHan0809 <kimhangyeol14@gmail.com>
2023-01-10 15:38:15 +0900

    all plot
    all plot

--- plot_learning_curve_result_even.py
+++ plot_learning_curve_result_even.py
@@ -43,7 +43,7 @@ if __name__ == "__main__":
 
 		plt.subplot(3, 3, idx + 1,)
 		if idx == 8:
-			plt.plot(x, mean, linewidth=0.5, label="TD3+IABC")
+			plt.plot(x, mean, linewidth=0.5, label="TD3+IABC (our)")
 		else:
 			plt.plot(x, mean, linewidth=0.5)
 		plt.fill_between(x,mean-std/div_std, mean+std/div_std, alpha=0.2)
@@ -53,6 +53,34 @@ if __name__ == "__main__":
 			plt.ylabel("Normalized Returns",fontsize=10)
 		plt.grid()
 		plt.subplots_adjust(left=0.2, bottom=0.1, right=1.2, top=0.9, wspace=0.3, hspace=0.4)
+
+	algo = "TD3_ABC/"
+
+	p_dir = "./results/"+algo
+	ext = ".npy"
+	total_sum = 0
+	for idx,env in enumerate(envs):
+		data = []
+		for file_name in os.listdir(p_dir):
+			if env in file_name:
+				data.append(np.load(p_dir+file_name))
+			else:
+				pass
+		mean = np.array(data).mean(axis=0)[0::2]
+		std  = np.array(data).std(axis=0)[0::2]
+		total_sum += mean[-1]
+
+		plt.subplot(3, 3, idx + 1)
+		if idx==8:
+			plt.plot(x,mean,linewidth=0.5,label="TD3+ABC (our)")
+		else:
+			plt.plot(x, mean, linewidth=0.5)
+		plt.fill_between(x,mean-std/div_std, mean+std/div_std, alpha=0.2)
+		plt.title(env,fontsize=10)
+		plt.xlabel("Training step",fontsize=10)
+		if idx == 0:
+			plt.ylabel("Normalized Returns",fontsize=10)
+		plt.subplots_adjust(left=0.125, bottom=0.1, right=0.9, top=0.9, wspace=0.3, hspace=0.4)
 
 	algo = "TD3_BC/"
 
@@ -138,61 +166,33 @@ if __name__ == "__main__":
 			plt.ylabel("Normalized Returns",fontsize=10)
 		plt.subplots_adjust(left=0.125, bottom=0.1, right=0.9, top=0.9, wspace=0.3, hspace=0.4)
 
-	algo = "TD3_IABCwDprime/"
+	# algo = "only_TD3_IABC/"
 
-	p_dir = "./results/"+algo
-	ext = ".npy"
-	total_sum = 0
-	for idx,env in enumerate(envs):
-		data = []
-		for file_name in os.listdir(p_dir):
-			if env in file_name:
-				data.append(np.load(p_dir+file_name))
-			else:
-				pass
-		mean = np.array(data).mean(axis=0)[0::2]
-		std  = np.array(data).std(axis=0)[0::2]
-		total_sum += mean[-1]
-
-		plt.subplot(3, 3, idx + 1)
-		if idx==8:
-			plt.plot(x,mean,linewidth=0.5,label="TD3+IABC with D'")
-		else:
-			plt.plot(x, mean, linewidth=0.5)
-		plt.fill_between(x,mean-std/div_std, mean+std/div_std, alpha=0.2)
-		plt.title(env,fontsize=10)
-		plt.xlabel("Training step",fontsize=10)
-		if idx == 0:
-			plt.ylabel("Normalized Returns",fontsize=10)
-		plt.subplots_adjust(left=0.125, bottom=0.1, right=0.9, top=0.9, wspace=0.3, hspace=0.4)
-
-	algo = "only_TD3_IABC/"
-
-	p_dir = "./results/"+algo
-	ext = ".npy"
-	total_sum = 0
-	for idx,env in enumerate(envs):
-		data = []
-		for file_name in os.listdir(p_dir):
-			if env in file_name:
-				data.append(np.load(p_dir+file_name))
-			else:
-				pass
-		mean = np.array(data).mean(axis=0)[0::2]
-		std  = np.array(data).std(axis=0)[0::2]
-		total_sum += mean[-1]
-
-		plt.subplot(3, 3, idx + 1)
-		if idx==8:
-			plt.plot(x,mean,linewidth=0.5,label="only TD3+IABC")
-		else:
-			plt.plot(x, mean, linewidth=0.5)
-		plt.fill_between(x,mean-std/div_std, mean+std/div_std, alpha=0.2)
-		plt.title(env,fontsize=10)
-		plt.xlabel("Training step",fontsize=10)
-		if idx == 0:
-			plt.ylabel("Normalized Returns",fontsize=10)
-		plt.subplots_adjust(left=0.125, bottom=0.1, right=0.9, top=0.9, wspace=0.3, hspace=0.4)
+	# p_dir = "./results/"+algo
+	# ext = ".npy"
+	# total_sum = 0
+	# for idx,env in enumerate(envs):
+	# 	data = []
+	# 	for file_name in os.listdir(p_dir):
+	# 		if env in file_name:
+	# 			data.append(np.load(p_dir+file_name))
+	# 		else:
+	# 			pass
+	# 	mean = np.array(data).mean(axis=0)[0::2]
+	# 	std  = np.array(data).std(axis=0)[0::2]
+	# 	total_sum += mean[-1]
+	#
+	# 	plt.subplot(3, 3, idx + 1)
+	# 	if idx==8:
+	# 		plt.plot(x,mean,linewidth=0.5,label="only TD3+IABC")
+	# 	else:
+	# 		plt.plot(x, mean, linewidth=0.5)
+	# 	plt.fill_between(x,mean-std/div_std, mean+std/div_std, alpha=0.2)
+	# 	plt.title(env,fontsize=10)
+	# 	plt.xlabel("Training step",fontsize=10)
+	# 	if idx == 0:
+	# 		plt.ylabel("Normalized Returns",fontsize=10)
+	# 	plt.subplots_adjust(left=0.125, bottom=0.1, right=0.9, top=0.9, wspace=0.3, hspace=0.4)
 
 
 	plt.legend(loc='lower left')
